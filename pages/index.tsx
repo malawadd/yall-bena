d75eb49cbed66670f96eb28e3992f9642d68acaf
malawadd <50057882+malawadd@users.noreply.github.com>
2022-10-09 18:00:33 +0300

    🛑 fixed typo error

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <div className="text-primary text-center flex flex-col justify-center items-center">
         <h1 className="text-4xl font-sans font-bold">Connect your wallet</h1>
         <p className="text-sm text-secondary font-sans font-normal mt-3 mb-6">
-          In order to use the dApp's full functionality you have
+          In order to use this dApp full functionality you have
           <br />
           to connect it with your wallet.
         </p>
